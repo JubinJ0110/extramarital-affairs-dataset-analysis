fa86cef2103eece8aade9f98f9d9275d1775bb43
Jubin Joseph <jubinjos0110@gmail.com>
2025-01-02 12:59:18 -0600

    Add files via upload

--- AffairsDatasetExample.R
+++ AffairsDatasetExample.R
@@ -2,25 +2,18 @@ data("Affairs")
 library(AER)
 library(graphics)
 print(summary(Affairs))
-## Greene (2003)
-## Tab. 22.3 and 22.4
-fm_ols <- lm(affairs ~ age + yearsmarried + religiousness + occupation + rating,
-             data = Affairs)
-fm_probit <- glm(I(affairs > 0) ~ age + yearsmarried + religiousness + occupation + rating,
-                 data = Affairs, family = binomial(link = "probit"))
-fm_tobit <- tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating,
-                  data = Affairs)
-fm_tobit2 <- tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating,
-                   right = 4, data = Affairs)
-fm_pois <- glm(affairs ~ age + yearsmarried + religiousness + occupation + rating,
-               data = Affairs, family = poisson)
+
+fm_ols <- lm(affairs ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs)
+fm_probit <- glm(I(affairs > 0) ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs, family = binomial(link = "probit"))
+fm_tobit <- tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs)
+fm_tobit2 <- tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating, right = 4, data = Affairs)
+fm_pois <- glm(affairs ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs, family = poisson)
+
 library("MASS")
-fm_nb <- glm.nb(affairs ~ age + yearsmarried + religiousness + occupation + rating,
-                data = Affairs)
-## Tab. 22.6
+fm_nb <- glm.nb(affairs ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs)
+
 library("pscl")
-fm_zip <- zeroinfl(affairs ~ age + yearsmarried + religiousness + occupation + rating | age +
-                     yearsmarried + religiousness + occupation + rating, data = Affairs)
+fm_zip <- zeroinfl(affairs ~ age + yearsmarried + religiousness + occupation + rating | age + yearsmarried + religiousness + occupation + rating, data = Affairs)
 
 
 print(summary(fm_ols))
